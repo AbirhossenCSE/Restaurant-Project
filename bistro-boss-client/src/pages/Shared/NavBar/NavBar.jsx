c661ff2628b52387f14652a37da62a7d5e34f628
Abir Hossen <abir.cse4@gmail.com>
2025-01-05 00:09:14 +0600

    react responsive carousel

--- bistro-boss-client/src/pages/Shared/NavBar/NavBar.jsx
+++ bistro-boss-client/src/pages/Shared/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar w-11/12 fixed z-10 bg-opacity-30 bg-gray-400 text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
