36d85a847c4d4398a29fbd6c16f99c79f37ea262
Abir Hossen <abir.cse4@gmail.com>
2025-01-06 17:25:32 +0600

    Parallax and react rating

--- bistro-boss-client/src/pages/Home/Featured/Featured.jsx
+++ bistro-boss-client/src/pages/Home/Featured/Featured.jsx
@@ -5,12 +5,12 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className='featured-item text-white pt-6'>
+        <div className='featured-item bg-fixed text-white pt-6 my-20'>
             <SectionTitle
             subHeading='Check it Out'
             heading='Featured Item'
             ></SectionTitle>
-            <div className='md:flex justify-center items-center pb-20 pt-12 px-36'>
+            <div className='md:flex justify-center items-center bg-slate-500 bg-opacity-30 pb-20 pt-12 px-36'>
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -18,7 +18,7 @@ const Featured = () => {
                     <p>Aug 20, 2030</p>
                     <p className='uppercase'>Where can i get some food</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex harum reiciendis quo omnis totam eligendi quod nam. Doloribus, velit. Maiores sint, perferendis sunt veritatis provident voluptates omnis laboriosam quis ad!</p>
-                    <button className='btn btn-outline'>Order Now</button>
+                    <button className='btn btn-outline border-0 border-b-4'>Order Now</button>
                 </div>
             </div>
         </div>
