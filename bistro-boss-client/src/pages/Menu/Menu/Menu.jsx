9def3ac67a378c577a7a3e48e7f4d03f4633e9af
Abir Hossen <abir.cse4@gmail.com>
2025-01-06 19:10:48 +0600

    Create Custom Hook

--- bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
+++ bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
 
 const Menu = () => {
     return (
@@ -11,11 +11,7 @@ const Menu = () => {
                 <title>Rastaurant | Menu</title>
             </Helmet>
             <Cover img={menuImg} title='our menu'></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title='our menu'></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title='our menu'></Cover>
-            <PopularMenu></PopularMenu>
+
         </div>
     );
 };
