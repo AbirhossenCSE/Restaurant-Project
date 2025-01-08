5425cc9899c714cc667fa133f573cc4b4d38f44c
Abir Hossen <abir.cse4@gmail.com>
2025-01-08 22:39:43 +0600

    login before add to the cart

--- bistro-boss-client/src/components/FoodCard/FoodCard.jsx
+++ bistro-boss-client/src/components/FoodCard/FoodCard.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
 
 const FoodCard = ({ item }) => {
     const { name, image, price, recipe } = item;
+    const {user} = useAuth();
+    const navigate = useNavigate();
+
     const handleAddToCart = food =>{
-        console.log(food);
+        if (user && user.email) {
+            // send to DB
+        }
+        else{
+            Swal.fire({
+                title: "You are not logged in",
+                text: "Please login",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Login!"
+              }).then((result) => {
+                if (result.isConfirmed) {
+                //   send user to login page
+                navigate('/login')
+                }
+              });
+        }
         
     }
 
