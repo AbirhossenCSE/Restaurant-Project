cab13b8d7ba9c659ed3f46017a29e75727af2b48
Abir Hossen <abir.cse4@gmail.com>
2025-01-09 20:51:23 +0600

    add

--- bistro-boss-client/src/SignUp/SignUp.jsx
+++ bistro-boss-client/src/SignUp/SignUp.jsx
@@ -4,37 +4,49 @@ import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider';
 import Swal from 'sweetalert2';
+import useAxiosPublic from '../hooks/useAxiosPublic';
 
 
 const SignUp = () => {
     const navigate = useNavigate();
+    const axiosPublic = useAxiosPublic();
     const {
         register,
         handleSubmit,
         reset,
         formState: { errors },
     } = useForm();
-    const {createUser, updateUserProfile} = useContext(AuthContext);
+    const { createUser, updateUserProfile } = useContext(AuthContext);
 
     const onSubmit = (data) => {
         console.log(data);
         createUser(data.email, data.password)
-        .then(result => {
-            const loggedUser = result.user;
-            console.log(loggedUser);
-            updateUserProfile(data.name, data.photoURL)
-            .then(() => {
-                reset();
-                Swal.fire({
-                    position: "top-end",
-                    icon: "success",
-                    title: "Your Profile has been updated",
-                    showConfirmButton: false,
-                    timer: 1500
-                  });
-                  navigate('/')
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                updateUserProfile(data.name, data.photoURL)
+                    .then(() => {
+                        const userInfo = {
+                            name: data.name,
+                            email: data.email
+                        }
+                        axiosPublic.post('/users', userInfo)
+                            .then(res => {
+                                if (res.data.insertedId) {
+                                    reset();
+                                    Swal.fire({
+                                        position: "top-end",
+                                        icon: "success",
+                                        title: "Your Profile has been updated",
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    });
+                                    navigate('/')
+                                }
+                            })
+
+                    })
             })
-        })
     };
 
     return (
