134cb1a4b46ada208955753ae6b4a8456bcf75e7
Abir Hossen <abir.cse4@gmail.com>
2025-01-10 15:10:15 +0600

    Save User if he doesn’t exists in the database

--- bistro-boss-client/src/Layout/Dashboard.jsx
+++ bistro-boss-client/src/Layout/Dashboard.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { FaAd, FaCalendar, FaHome, FaList, FaShoppingCart } from 'react-icons/fa';
+import { FaAd, FaCalendar, FaEnvelope, FaHome, FaList, FaShoppingCart } from 'react-icons/fa';
 import { MdBorderColor, MdMenu, MdOutlineRateReview } from 'react-icons/md';
 import { NavLink, Outlet } from 'react-router-dom';
 import useCart from '../hooks/useCart';
 
 const Dashboard = () => {
     const [cart] = useCart();
+
+    // TODO: get isAdmin value from the daatabase
+    const isAdmin = true;
+
+    
     return (
         <div className='flex'>
             {/* Dashboard sidebar */}
@@ -21,11 +26,13 @@ const Dashboard = () => {
 
                     <li><NavLink to='/dashboard/bookings'> <FaList></FaList> My Booking</NavLink></li>
 
+                    {/* Shared NavLinks */}
                     <div className='divider'></div>
 
                     <li><NavLink to='/'> <FaHome></FaHome> Home</NavLink></li>
                     <li><NavLink to='/menu'> <MdMenu /> Menu</NavLink></li>
                     <li><NavLink to='/order/salad'> <MdBorderColor /> Order</NavLink></li>
+                    <li><NavLink to='/order/contact'> <FaEnvelope /> Contact</NavLink></li>
                 </ul>
             </div>
             {/* dashboard content */}
