949f7477f83d74298b07c1ea92f4439bd221ce27
Abir Hossen <abir.cse4@gmail.com>
2025-01-12 15:25:21 +0600

    unauthorized access

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -42,14 +42,14 @@ async function run() {
     })
     // middleware----------JWT-3
     const verifyToken = (req, res, next) => {
-      // console.log('Inside VerifyToken', req.headers.authorization);
+      console.log('Inside VerifyToken', req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({message: 'forbidden access'});
+        return res.status(401).send({ message: 'forbidden access' });
       }
       const token = req.headers.authorization.split(' ')[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({message: 'forbidden-access'})
+          return res.status(401).send({ message: 'forbidden-access' })
         }
         req.decoded = decoded;
         next();
@@ -95,6 +95,20 @@ async function run() {
       }
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
+    })
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorised access' })
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+
+      }
+      res.send({ admin })
     })
 
 
