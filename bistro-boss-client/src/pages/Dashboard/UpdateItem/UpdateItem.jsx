399c55fceda8952a17e22574f12cea7d81ac5a3c
Abir Hossen <abir.cse4@gmail.com>
2025-01-13 20:45:57 +0600

    upadte item

--- bistro-boss-client/src/pages/Dashboard/UpdateItem/UpdateItem.jsx
+++ bistro-boss-client/src/pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -5,13 +5,14 @@ import { useForm } from 'react-hook-form';
 import useAxiosPublic from '../../../hooks/useAxiosPublic';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { FaUtensils } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const UpdateItem = () => {
     // const { item } = useLoaderData();
-    const { name, category, recipe, price } = useLoaderData();
+    const { name, category, recipe, price, _id } = useLoaderData();
     // const {name, category, recipe, price} = item();
     // console.log('loded item', item);
     // 69-8-12min vdo watch
@@ -38,18 +39,18 @@ const UpdateItem = () => {
                 recipe: data.recipe,
                 image: res.data.data.display_url
             }
-            const menuRes = await axiosSecure.patch('/menu', menuItem)
+            const menuRes = await axiosSecure.patch(`/menu/${_id}`, menuItem)
             // console.log(menuRes.data);
-            if (menuRes.data.insertedId) {
+            if (menuRes.data.modifiedCount > 0) {
                 // success popup
                 Swal.fire({
                     position: "top-end",
                     icon: "success",
-                    title: `${data.name} is added to menu`,
+                    title: `${data.name} is upadted to menu`,
                     showConfirmButton: false,
                     timer: 1500
                 });
-                reset();
+                // reset();
             }
         }
     };
