6d1b00e5a7947f5bb2795bf9b410d17e6f74e58c
Abir Hossen <abir.cse4@gmail.com>
2025-01-14 12:11:29 +0600

    stripe

--- bistro-boss-client/src/pages/Dashboard/Payment/Payment.jsx
+++ bistro-boss-client/src/pages/Dashboard/Payment/Payment.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
+// TODO: Add publishable kay
+const stripePromise = loadStripe('');
 const Payment = () => {
     return (
         <div>
-           <SectionTitle heading='payment' subHeading='Please Pay Bill'></SectionTitle> 
-           <div>
-            <h2 className='text-4xl'>Chose your payment system</h2>
-           </div>
+            <SectionTitle heading='payment' subHeading='Please Pay Bill'></SectionTitle>
+            <div>
+                <Elements stripe={stripePromise}>
+
+                </Elements>
+            </div>
         </div>
     );
 };
