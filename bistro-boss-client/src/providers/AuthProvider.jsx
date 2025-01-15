ff917a28d7bcb24356ff92f22174f052a3fdc16e
Abir Hossen <abir.cse4@gmail.com>
2025-01-15 21:33:25 +0600

    API race condition

--- bistro-boss-client/src/providers/AuthProvider.jsx
+++ bistro-boss-client/src/providers/AuthProvider.jsx
@@ -49,15 +49,17 @@ const AuthProvider = ({ children }) => {
                     .then(res => {
                         if (res.data.token) {
                             localStorage.setItem('access-token', res.data.token);
+                            setLoading(false);
                         }
                     })
             }
             else {
                 // TODO: remove token (if token stored in client side: local storage, cashing, in memory)
                 localStorage.removeItem('access-token');
+                setLoading(false);
             }
 
-            setLoading(false);
+            // setLoading(false);
         })
         return () => {
             return () => {
