a069367676a9966d094f3f88f941736bc9178368
Abir Hossen <abir.cse4@gmail.com>
2025-01-15 21:12:12 +0600

    dashboard hpme

--- bistro-boss-client/src/Routes/Routes.jsx
+++ bistro-boss-client/src/Routes/Routes.jsx
@@ -18,6 +18,8 @@ import ManageItems from "../pages/Dashboard/ManageItems/ManageItems";
 import UpdateItem from "../pages/Dashboard/UpdateItem/UpdateItem";
 import Payment from "../pages/Dashboard/Payment/Payment";
 import PaymentHistory from "../pages/Dashboard/PaymentHistory/PaymentHistory";
+import UserHome from "../pages/Dashboard/UserHome/UserHome";
+import AdminHome from "../pages/Dashboard/AdminHome/AdminHome";
 
 export const router = createBrowserRouter([
   {
@@ -56,6 +58,10 @@ export const router = createBrowserRouter([
     children: [
       // normal user
       {
+        path: 'userHome',
+        element: <UserHome></UserHome>,
+      },
+      {
         path: 'cart',
         element: <Cart></Cart>,
       },
@@ -69,6 +75,10 @@ export const router = createBrowserRouter([
       },
 
       // Admin Routes
+      {
+        path: 'adminHome',
+        element: <AdminRoute><AdminHome></AdminHome></AdminRoute>
+      },
       {
         path: 'users',
         element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
