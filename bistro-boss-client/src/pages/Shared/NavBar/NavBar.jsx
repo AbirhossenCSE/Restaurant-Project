880540d25a343f5ceb48883320f4f5b4ba8b667d
Abir Hossen <abir.cse4@gmail.com>
2025-01-15 21:18:56 +0600

    Dashboard Home page for User and Admin

--- bistro-boss-client/src/pages/Shared/NavBar/NavBar.jsx
+++ bistro-boss-client/src/pages/Shared/NavBar/NavBar.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../providers/AuthProvider';
 import { FiShoppingCart } from 'react-icons/fi';
 import useCart from '../../../hooks/useCart';
+import useAdmin from '../../../hooks/useAdmin';
 
 const NavBar = () => {
     const { user, logOut } = useContext(AuthContext);
     const [cart] = useCart();
-    
+    const [isAdmin] = useAdmin();
 
     const handleLogOut = () => {
         logOut()
@@ -20,6 +21,12 @@ const NavBar = () => {
         <li><Link to='/menu'>Our Menu</Link></li>
         <li><Link to='/order/salad'>Order</Link></li>
         <li><Link to='/secret'>Secret</Link></li>
+        {
+            user && isAdmin && <li><Link to='/dashboard/adminHome'>Dashboard</Link></li>
+        }
+        {
+            user && !isAdmin && <li><Link to='/dashboard/userHome'>Dashboard</Link></li>
+        }
         <li>
             <Link to='/dashboard/cart'>
                 <button className="btn btn-xs">
